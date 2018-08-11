5f89790e52486794725e6e91860707fc37553016
Jonathan <jonathanconn29@gmail.com>
2018-08-11 12:30:06 -0400

    getNum v1
    generating a random 2 or 4

--- 2048/src/main/main.java
+++ 2048/src/main/main.java
@@ -1,8 +1,17 @@
 package main;
 
 public class main {
+	int[][] board = new int[3][3];
 	
 	public static void main(String[] args) {
-		
+		getNum();
 	}
+	public static int getNum() {
+		int newNumber;
+		if(Math.random() > 0.5) newNumber = 2;
+		else newNumber = 4;
+		return newNumber;
+	}
+	
+	
 }
