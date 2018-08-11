156c50f6737c77eaa45ed27ea63fb7260968f716
Jonathan <jonathanconn29@gmail.com>
2018-08-11 12:44:50 -0400

    Added printArray method
    prints all the contents of the board formatted

--- 2048/src/main/main.java
+++ 2048/src/main/main.java
@@ -1,10 +1,11 @@
 package main;
 
 public class main {
-	int[][] board = new int[3][3];
+	public static int rows = 4, columns = 4;
+	public static int[][] board = new int[rows][columns];
 	
 	public static void main(String[] args) {
-		getNum();
+		printArray(board);
 	}
 	public static int getNum() {
 		int newNumber;
@@ -12,6 +13,27 @@ public class main {
 		else newNumber = 4;
 		return newNumber;
 	}
+	
+	public static void addNum(int[][] array, int num) {
+		num = getNum();
+		int options = 0;
+		
+		for(int i = 0; i < rows; i++) {
+			for( int j = 0; j < columns; j++) {
+				if(array[i][j] == 0) options++;
+			}
+		}
+		
+	}
+	public static void printArray(int[][] array) {
+		for(int i = 0; i < rows; i++) {
+			for( int j = 0; j < columns; j++) {
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+	}
+	
 	
 	
 }
