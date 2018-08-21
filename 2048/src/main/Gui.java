b343cdc10c7fa5ea44b176a628ca9d5622bff6c6
Jonathan <jonathanconn29@gmail.com>
2018-08-20 21:48:46 -0400

    Grid layout framework

--- 2048/src/main/Gui.java
+++ 2048/src/main/Gui.java
@@ -1,34 +1,41 @@
 package main;
 
-import java.awt.Insets;
+import javafx.application.Application; 
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.layout.GridPane; 
+import javafx.scene.text.Text; 
+import javafx.scene.control.TextField; 
+import javafx.stage.Stage; 
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
+
 public class Gui extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        //lamda expression
-        btn.setOnAction(e -> System.out.println("test"));
+    public void start(Stage stage) {
+    	
+        Text title = new Text("2048");       
+      
+        GridPane gridPane = new GridPane();    
+         
+        gridPane.setMinSize(500,500); 
+         
+        gridPane.setPadding(new Insets(10, 10, 10, 10)); 
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 500, 500);
+        gridPane.setVgap(5); 
+        gridPane.setHgap(5);       
         
-        primaryStage.setTitle("2048");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        gridPane.setAlignment(Pos.CENTER); 
+         
+        gridPane.add(title, 0, 0); 
+        
+        Scene scene = new Scene(gridPane);  
+        
+        stage.setTitle("2048"); 
+        stage.setScene(scene); 
+        stage.show(); 
     }
     
     public static void callStart() {
